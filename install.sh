c60cf9277656944817f9411d2e5f886a718c1bae
D4SuCE <d4suce@gmail.com>
2024-12-16 16:32:25 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo cp anime-cli anime-skip /usr/local/bin
+sudo chmod +x /usr/local/bin/anime-cli /usr/local/bin/anime-skip
 mkdir -p ~/.config/mpv/scripts
 cp skip.lua ~/.config/mpv/scripts
