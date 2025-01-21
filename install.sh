2edf5d294df659277e790075c13a0e1793f9778d
D4SuCE <d4suce@gmail.com>
2025-01-21 03:38:00 +0300

    Added: man manual

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sudo cp anime-cli anime-skip /usr/local/bin
 sudo chmod +x /usr/local/bin/anime-cli /usr/local/bin/anime-skip
 mkdir -p ~/.config/mpv/scripts
 cp skip.lua ~/.config/mpv/scripts
+sudo cp anime-cli.1 /usr/share/man/man1
